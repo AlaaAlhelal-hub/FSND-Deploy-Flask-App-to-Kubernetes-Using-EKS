9f8ecf127ee5ff85a9925a6303ea1a450a796288
AlaaAlhelal <36053108+AlaaAlhelal@users.noreply.github.com>
2020-12-10 20:15:15 +0300

    v1

--- examples/flask/app.py
+++ examples/flask/app.py
@@ -1,6 +1,13 @@
 from flask import Flask
 APP = Flask(__name__)
 
+'''
+
+    GET '/': This is a simple health check, which returns the response 'Healthy'.
+    POST '/auth': This takes an email and password as json arguments and returns a JWT based on a custom secret.
+    GET '/contents': This requires a valid JWT, and returns the un-encrypted contents of that token.
+
+'''
 
 @APP.route('/')
 def hello_world():
